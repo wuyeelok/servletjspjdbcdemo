05f3107c881a3dc5086a0a3171f33d6ea1ad1bfb
wuyeelok <wuyeelok@gmail.com>
2022-02-24 15:38:50 +0800

    successfully use jdbc to connect to DB

--- src/main/java/com/kenneth/wu/Kenneth1.java
+++ src/main/java/com/kenneth/wu/Kenneth1.java
@@ -1,6 +1,10 @@
 package com.kenneth.wu;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +17,9 @@ public class Kenneth1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/employee?useSSL=false";
+	static final String USER = "edureka";
+	static final String PASSWORD = "edureka";
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -29,17 +36,28 @@ public class Kenneth1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		Boolean isClassFound = false;
+		Boolean isConnectDB = false;
+		Connection conn = null;
+
 		try {
+			// Register JDBC driver
 			Class.forName(JDBC_DRIVER);
-			isClassFound = true;
+
+			// Open a connection
+			conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+			isConnectDB = true;
 		} catch (ClassNotFoundException e) {
+			System.out.println("JDBC Driver not found");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Fail to connect to DB");
 			e.printStackTrace();
 		}
 
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath()).append("Class is found? ->")
-				.append(String.valueOf(isClassFound));
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("Connect to DB? -> ")
+				.append(String.valueOf(isConnectDB));
 	}
 
 	/**
